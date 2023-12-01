19f3b74d3dcf0b77de1789d195e80561a2d3bea2
massimorid <133862511+massimorid@users.noreply.github.com>
2023-12-01 12:53:06 +0100

    Update generateTower.py

--- generateTower.py
+++ generateTower.py
@@ -24,11 +24,26 @@ class JengaTower:
         
     def __repr__(self):
         return repr(self.layers)
+        
+    def render(self):
+        x = 0
+        y = 0
+        for layer in self.layers:
+            for piece in layer.pieces:
+                print(f"\033[{y};{x}H", end="")
+                print(piece, end="")
+                x += len(str(piece)) + 1
+            x = 0
+            y += 1
+        print()
 
 
-# height = 3
-# tower = JengaTower(height)
+class JengaPiece:
+    def __init__(self):
+        self.left = 0
+        self.middle = 0
+        self.right = 0
 
-# # Print each layer on a new line
-# for layer in tower.layers:
-#     print(layer)
+    def __repr__(self):
+        return f"[{self.left},{self.middle},{self.right}]"
+
